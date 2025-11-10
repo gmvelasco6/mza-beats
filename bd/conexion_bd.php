55ba17b76df34fe823eea466c4a07a3102c45d74
Gonzalo <gmvelasco6@gmail.com>
2025-11-10 17:36:53 -0300

    cambio sql a consulta 17.36 10-11

--- bd/conexion_bd.php
+++ bd/conexion_bd.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "login";
+$db = "mzabeats";
 // sirve para conectar a la base de datos
 $conexion=new mysqli($host,$user,$pass,$db);
 if($conexion->connect_error){
